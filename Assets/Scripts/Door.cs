fa3322954cace18d16a2400b772c6b5b9dda5c58
peabrainc64 <peabrainc64@hotmail.de>
2016-09-02 23:58:30 +0200

    a

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public GameObject door = null;
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enterd");
+        if (door != null)
+        {
+            Debug.Log("Play Anim");
+            if(!door.GetComponent<Animation>().isPlaying)
+                door.GetComponent<Animation>()["open"].normalizedTime = 0;
+            door.GetComponent<Animation>()["open"].speed = 1;
+            door.GetComponent<Animation>().Play("open");
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+//        Debug.Log("innen");
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        if (door != null)
+        {
+            Debug.Log("Play Anim");
+            if (!door.GetComponent<Animation>().isPlaying)
+                door.GetComponent<Animation>()["open"].normalizedTime = 1;
+            door.GetComponent<Animation>()["open"].speed = -1;
+            door.GetComponent<Animation>().Play("open");
+        }
+    }
+}
