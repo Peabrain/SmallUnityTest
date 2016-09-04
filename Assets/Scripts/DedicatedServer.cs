046e842ef7a8326ea7d763fd0358d2fdebdddd3c
peabrainc64 <peabrainc64@hotmail.de>
2016-09-04 14:45:09 +0200

    Dedicated Server

---
+++ Assets/Scripts/DedicatedServer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DedicatedServer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (SystemInfo.graphicsDeviceID == 0)
+        {
+            SetupServer();
+        }
+	}
+    void SetupServer() {
+        GetComponent<NetworkManager>().StartServer();
+    }	
+}
