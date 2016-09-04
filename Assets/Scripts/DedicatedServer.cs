9bceba3b33bb4de63c6adfc94226fd7a31926c7c
peabrainc64 <peabrainc64@hotmail.de>
2016-09-04 15:25:10 +0200

    b

--- Assets/Scripts/DedicatedServer.cs
+++ Assets/Scripts/DedicatedServer.cs
@@ -4,6 +4,7 @@ using UnityEngine.Networking;
 
 public class DedicatedServer : MonoBehaviour {
 
+    int status = 0;
 	// Use this for initialization
 	void Start () {
         if (SystemInfo.graphicsDeviceID == 0)
@@ -11,7 +12,26 @@ public class DedicatedServer : MonoBehaviour {
             SetupServer();
         }
 	}
+    void Update() {
+        NetworkManager nm = GetComponent<NetworkManager>();
+        if (status == 1) {
+            Debug.Log(nm.numPlayers);
+            if (nm.numPlayers > 0)
+            {
+                status = 2;
+            }
+        }
+        else
+        if (status == 2)
+        {
+            if (nm.numPlayers == 0) {
+                GetComponent<NetworkManager>().StopServer();
+                Application.Quit();
+            }
+        }
+    }
     void SetupServer() {
         GetComponent<NetworkManager>().StartServer();
+        status = 1;
     }	
 }
