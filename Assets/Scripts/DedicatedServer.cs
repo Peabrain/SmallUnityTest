1a131e7b3d34ac4a812020fb6e287895e2a03e76
peabrainc64 <peabrainc64@hotmail.de>
2016-09-11 23:31:02 +0200

    g

--- Assets/Scripts/DedicatedServer.cs
+++ Assets/Scripts/DedicatedServer.cs
@@ -1,6 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 using UnityEngine.Networking;
+
+/*public class DedicatedServer : MonoBehaviour
+{
+    private bool mRunning;
+    public static string msg = "";
+
+    public Thread mThread;
+    public TcpListener tcp_Listener = null;
+
+    void Awake()
+    {
+        mRunning = true;
+        ThreadStart ts = new ThreadStart(Receive);
+        mThread = new Thread(ts);
+        mThread.Start();
+        print("Thread done...");
+    }
+
+    public void stopListening()
+    {
+        mRunning = false;
+    }
+
+    void Receive()
+    {
+        tcp_Listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8001);
+        tcp_Listener.Start();
+        print("Server Start");
+        while (mRunning)
+        {
+            // check if new connections are pending, if not, be nice and sleep 100ms
+            if (!tcp_Listener.Pending())
+            {
+                Thread.Sleep(100);
+            }
+            else
+            {
+                Socket ss = tcp_Listener.AcceptSocket();
+//                BonePos rec = new BonePos();
+//                byte[] tempbuffer = new byte[10000];
+//                ss.Receive(tempbuffer); // received byte array from client
+//                rec.AssignFromByteArray(tempbuffer); // my own datatype
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnApplicationQuit()
+    { // stop listening thread
+        stopListening();// wait for listening thread to terminate (max. 500ms)
+        mThread.Join(500);
+    }
+}
+/**/
 
 public class DedicatedServer : MonoBehaviour {
 
